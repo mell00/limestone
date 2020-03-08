2a2e261fc0fa9b274617572930d437ecc92a7eb4
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-08 13:19:39 -0700

    Minor changes to database_functions file

--- database/database_functions.py
+++ database/database_functions.py
@@ -1,22 +1,23 @@
 import pandas as pd
+import ./components/form.js
 
 tripdetails = ["Photofile", "Team Leader", "Date", "Time", "Expected Out Time", "Call Out Time", "No. of Trip Members", "Return Time", "Trip Status", "Additional Info"]
 
-# write new trip data into table 
-def write_new(fromtheform):
+# write new trip data into table
+def write_new():
   df = pd.read_csv('csvfilepath')
-  
+
   # how to store info fromtheform into somearray
-  
+
   # second dataframe for new row
   df_new = pd.DataFrame(data=somearray, columns=tripdetails)
-  
-  # add new row to original data                
+
+  # add new row to original data
   df.append(df_new)
-  
+
   # update csv file
   trips_df.to_csv
-  
+
   return
 
 # read row at index
@@ -25,7 +26,7 @@ def read(index):
 
   # store in multiple variables?
   # photofile = df.loc[index, 'Photofile']
-  # ... 
-  
+  # ...
+
   # store in one dataframe
   df_read = pd.DataFrame(data=df.loc([index], columns=tripdetails)
